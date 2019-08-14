4e3c48a21af6733c3ba9cc671d0d490a9ad0d154
mick19955 <mikkel.a.b.p@gmail.com>
2019-08-14 20:24:41 +0200

    Update client.py

--- Single_Client/client.py
+++ Single_Client/client.py
@@ -9,7 +9,7 @@ def socket_create():
         global host
         global port
         global s
-        host = '192.168.0.5'
+        host = '10.10.10.139'
         port = 9999
         s = socket.socket()
     except socket.error as msg:
